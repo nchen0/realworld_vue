54e26b1a1c40161184290c84ef8a2443ecaae42e
Nicky Chen <nchen@Nicky-Work-Macbook-Pro.local>
2019-01-17 14:21:19 -0500

    vue router

--- real-world-vue/src/router.js
+++ real-world-vue/src/router.js
@@ -1,24 +1,36 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import EventList from "./views/EventList.vue";
+import EventCreate from "./views/EventCreate.vue";
+import EventShow from "./views/EventShow.vue";
+import User from "./views/User.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "eventlist",
+      component: EventList
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/create",
+      name: "eventcreate",
+      component: EventCreate
+    },
+    {
+      path: "/event/:id",
+      name: "eventshow",
+      component: EventShow,
+      props: true
+    },
+    {
+      path: "/user/:username",
+      name: "user",
+      component: User,
+      props: true
     }
   ]
 });
---
+++ real-world-vue/src/views/EventList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <router-link :to="{name: 'eventshow', params: {id: '1'}}">Show View 1</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
